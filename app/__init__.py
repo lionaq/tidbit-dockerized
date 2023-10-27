bb0b19edbb57466c2baa689fabd7c91bfdb9e3f1
Nancy Sayson <nancy.sayson@g.msuiit.edu.ph>
2023-10-27 10:20:18 +0800

    modified the directory

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+from flask_wtf.csrf import CSRFProtect
 
 mysql = MySQL()
 bootstrap = Bootstrap()
--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
 
 SECRET_KEY = getenv("SECRET_KEY")
 DB_NAME = getenv("DB_NAME")
 DB_USERNAME = getenv("DB_USERNAME")
-DB_PASSWORD = "root"
+DB_PASSWORD = getenv("DB_PASSWORD")
 DB_HOST = getenv("DB_HOST")
 BOOTSTRAP_SERVE_LOCAL = getenv("BOOTSTRAP_SERVE_LOCAL")
