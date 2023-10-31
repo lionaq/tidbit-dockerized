9add4d3b8ba78e354d0ab6b7bc827e94f653fd6f
John Lenard Requina <lenard147896325@gmail.com>
2023-10-31 21:47:41 +0800

    added sql schema

---
+++ flask.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS `tidbit`;
+CREATE DATABASE IF NOT EXISTS `tidbit`;
+USE `tidbit`;
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    fullname VARCHAR(255),
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    bio TEXT,
+    profilepic VARCHAR(255),
+    coverpic VARCHAR(255)
+);
