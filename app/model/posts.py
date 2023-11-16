59255dde8889af2f4a973ef81996e4661d340173
Johnaif Abdul <johnaif.abdul@g.msuiit.edu.ph>
2023-11-16 20:33:36 +0800

    TID-18 fix: Fixed a typo in the models query for adding posts

--- app/model/posts.py
+++ app/model/posts.py
@@ -17,11 +17,11 @@ class Post(UserMixin):
 
     def add(self):
         cursor = mysql.connection.cursor(dictionary=True)
-        sql_post = "INSERT INTO post(user_id, date, title, caption, ingredients, instructions, tag, subtags, type) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql_post = "INSERT INTO post(user_id, date, title, caption, ingredients, instructions, tag, subtags) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         sql_urls = "INSERT INTO post_url (post_id, url, type) VALUES (%s, %s, %s)"
 
         # Insert post data into the `post` table
-        cursor.execute(sql_post, (self.user_id, self.date, self.title, self.caption, self.ingredients, self.instructions, self.tag, self.subtags, self.type))
+        cursor.execute(sql_post, (self.user_id, self.date, self.title, self.caption, self.ingredients, self.instructions, self.tag, self.subtags))
 
         # Get the post_id of the inserted post
         post_id = cursor.lastrowid
