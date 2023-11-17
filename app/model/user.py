7342fc01ca6261e5588e9cb77f138c7362472d97
Nancy Sayson <nancy.sayson@g.msuiit.edu.ph>
2023-11-17 13:20:19 +0800

    Updated default pics, icons with Flask's 'url_for'

--- app/model/user.py
+++ app/model/user.py
@@ -1,6 +1,7 @@
 from app import mysql, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask import url_for
 
 @login.user_loader
 def load_user(id):
@@ -14,8 +15,8 @@ class User(UserMixin):
         self.username = username
         self.password = password
         self.bio = bio
-        self.profilepic = profilepic if profilepic else 'static/img/default_profilepic.png'
-        self.coverpic = coverpic if coverpic else 'static/img/default_coverpic.jpg'
+        self.profilepic = profilepic if profilepic else  url_for('static', filename='img/default_profilepic.png')
+        self.coverpic = coverpic if coverpic else url_for('static', filename='img/default_coverpic.jpg')
         
     def set_password(self, password):
         self.password = generate_password_hash(password)
