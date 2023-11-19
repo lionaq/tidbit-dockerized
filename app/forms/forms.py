5c3a0b1a2e8a1fdb441d73dd9c8e25b173b0f79d
Romeo Giullano R. Aclo <romeogiullano.aclo@g.msuiit.edu.ph>
2023-11-20 00:11:15 +0800

    TID-19 fix: image edit improper

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -39,7 +39,7 @@ class CreatePost(FlaskForm):
     submit = SubmitField('Create Post')
 
 class EditPost(FlaskForm):
-    content = FileField('Content', validators=[FileAllowed(['jpg', 'png', 'gif', 'mp4', 'mov']), validators.Optional()], render_kw={'multiple': True})
+    content = MultipleFileField('Content', validators=[FileAllowed(['jpg', 'png', 'gif', 'mp4', 'mov']), validators.Optional()], render_kw={'multiple': True})
     title = StringField('Title', [validators.DataRequired()])
     caption = TextAreaField('Caption', [validators.DataRequired()] )
     ingredients = TextAreaField('Ingredients', [validators.DataRequired()])
